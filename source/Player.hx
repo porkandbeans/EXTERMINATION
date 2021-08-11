6a90de97901fb3bd0a11e2f0de6d3251147e6c42
Charlie <voter96@gmail.com>
2021-08-12 11:58:37 +1200

    fixed hitbox, added melee animation and logic

--- source/Player.hx
+++ source/Player.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.system.debug.stats.StatsGraph;
+import flixel.util.FlxTimer;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
@@ -11,28 +13,30 @@ class Player extends FlxSprite
     var _weight:Float = 300;
     var _grounded:Bool = true;
 	var _canJump:Bool;
+	var _canAttack:Bool;
+	var _attacking:Bool;
 
 	var MAX_JUMPHOLD = 20;
 
     public function new(x:Float = 0, y:Float = 0)
     {
         super(x,y); // passes x and y to FlxSprite
-        
+
         maxVelocity.set(160, 200);
         acceleration.y = _weight;
         drag.x = maxVelocity.x * 5;
 
-		setSize(16, 30);
-		offset.set(8, 0);
-
+		_canAttack = true;
 		_jumpHold = MAX_JUMPHOLD;
 
-
 		loadGraphic("assets/images/Player/player.png", true, 32, 32);
+		setSize(16, 32);
+		offset.set(8, 0);
 		animation.add("idle", [0]);
 		animation.add("run", [1, 2, 3, 4, 5, 6, 7, 8], 9, true);
 		animation.add("jump", [9, 10], 4, false);
 		animation.add("fall", [11, 12], 9, true);
+		animation.add("melee", [13, 14, 15], 12, false); 
     }
 
 	override public function update(elapsed:Float)
@@ -53,51 +57,72 @@ class Player extends FlxSprite
 	}
 
 	function keyListeners(){
-		if (FlxG.keys.anyPressed([LEFT, A])){
-			acceleration.x = -maxVelocity.x * 7;
-		}
+		if(!_attacking){
+			if (FlxG.keys.anyPressed([LEFT, A])){
+				acceleration.x = -maxVelocity.x * 7;
+			}
 
-		if (FlxG.keys.anyPressed([RIGHT, D])){
-			acceleration.x = maxVelocity.x * 7;
-		}
-		
-		// fixes being able to spam jump while in the air
-		if(FlxG.keys.anyJustReleased([SPACE, W, UP]) && !isTouching(FlxObject.FLOOR)){
-			_canJump = false;
-		}
+			if (FlxG.keys.anyPressed([RIGHT, D])){
+				acceleration.x = maxVelocity.x * 7;
+			}
+			
+			// fixes being able to spam jump while in the air
+			if(FlxG.keys.anyJustReleased([SPACE, W, UP]) && !isTouching(FlxObject.FLOOR)){
+				_canJump = false;
+			}
 
-		if(isTouching(FlxObject.FLOOR)){
-			_canJump = true;
-		}
+			if(isTouching(FlxObject.FLOOR)){
+				_canJump = true;
+			}
+	
+			if (FlxG.keys.anyPressed([SPACE, W, UP]) && velocity.y <= 0 && _canJump){
+				if(_jumpHold > 0){
+					_jumpHold -= 1;
+					velocity.y = -_jumpForce;
+				}
+			}
 
-		if (FlxG.keys.anyPressed([SPACE, W, UP]) && velocity.y <= 0 && _canJump){
-			if(_jumpHold > 0){
-				_jumpHold -= 1;
-				velocity.y = -_jumpForce;
+			if(FlxG.keys.anyPressed([ENTER]) && _canAttack){
+				_attacking = true;
+				_canAttack = false;
+				animation.play("melee");
+				new FlxTimer().start(0.5, finishAttacking, 1);
 			}
 		}
     }
 
 	function animations(){
-		if(velocity.y == 0){
-			if (velocity.x == 0){
-				animation.play("idle");
-			}
-			else{
-				animation.play("run");
-			}
-		}else{
-			if(velocity.y > -0.7){
-				animation.play("fall");
-			}else if(isTouching(FlxObject.FLOOR)){
-				animation.play("jump");
+		if(!_attacking){
+			if(velocity.y == 0){
+				if (velocity.x == 0){ 
+					animation.play("idle");
+				}
+				else{
+					animation.play("run");
+				}
+			}else{
+				if(velocity.y > -0.7){
+					animation.play("fall");
+				}else if(isTouching(FlxObject.FLOOR)){
+					animation.play("jump");
+				}
 			}
 		}
 
-		if(velocity.x > 0){
-			flipX = false;
-		}else if(velocity.x < 0){
+		if (FlxG.keys.anyPressed([LEFT, A])){
 			flipX = true;
 		}
+
+		if (FlxG.keys.anyPressed([RIGHT, D])){
+			flipX = false;
+		}
+
+		if(animation.curAnim == animation.getByName("melee") && animation.finished){
+			_attacking = false;
+		}
+	}
+
+	function finishAttacking(timer:FlxTimer):Void {
+		_canAttack = true;
 	}
 }
