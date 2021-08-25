c396c12b29faceb5fb33e1fc13585407bea9e94e
Charlie <voter96@gmail.com>
2021-08-25 12:58:38 +1200

    Added some more stuff to the main menu

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -17,6 +17,12 @@ class MenuState extends FlxState
 	var _continueButton:FlxButton;
 	var _levelsButton:FlxButton;
 
+	var _optionsMenu:FlxGroup;
+	var _optionsCloseButton:FlxButton;
+	var _volumeText:FlxButton;
+	var _volUpButton:FlxButton;
+	var _volDownButton:FlxButton;
+
 	var _menuMusic:FlxSound;
 
 	var _screenWidth:Float;
@@ -50,7 +56,20 @@ class MenuState extends FlxState
 		_mainMenu.add(_levelsButton);
 		_mainMenu.add(_optionsButton);
 
+		_optionsCloseButton = new FlxButton(0, FlxG.height / 2, "Back", closeOptions);
+		add(_optionsCloseButton);
+		_volUpButton = new FlxButton(_buttonWidth, 90, '+', volumeUp);
+		add(_volUpButton);
+		_volDownButton = new FlxButton(_buttonWidth - 80, 90, '-', volumeDown);
+		add(_volDownButton);
+
+		_optionsMenu = new FlxGroup();
+		_optionsMenu.add(_optionsCloseButton);
+		_optionsMenu.add(_volUpButton);
+		_optionsMenu.add(_volDownButton);
+
 		_mainMenu.forEach(hideButton);
+		_optionsMenu.forEach(hideButton);
 
 		_menuMusic = FlxG.sound.load("assets/music/menu.mp3", 1, // volume
 			true, // looped
@@ -65,40 +84,47 @@ class MenuState extends FlxState
 		super.create();
 	}
 
-	function hideButton(button:FlxBasic)
-	{
+	function hideButton(button:FlxBasic){
 		button.visible = false;
 	}
 
-	function showButton(button:FlxBasic)
-	{
+	function showButton(button:FlxBasic){
 		button.visible = true;
 	}
 
-	function clickPlay()
-	{
+	function clickPlay(){
 		_startButton.visible = false;
 		_mainMenu.forEach(showButton);
 	}
 
-	function newGame()
-	{
+	function newGame(){
 		FlxG.sound.music.pause();
 		FlxG.switchState(new PlayState());
 	}
 
-	function optionsMenu()
-	{
+	function optionsMenu(){
+		_mainMenu.forEach(hideButton);
+		_optionsMenu.forEach(showButton);
+	}
+
+	function closeOptions(){
+		_optionsMenu.forEach(hideButton);
+		_mainMenu.forEach(showButton);
+	}
+
+	function continueGame(){
 		// this doesn't do anything right now
 	}
 
-	function continueGame()
-	{
+	function levelsMenu(){
 		// this doesn't do anything right now
 	}
 
-	function levelsMenu()
-	{
-		// this doesn't do anything right now
+	function volumeUp(){
+
+	}
+
+	function volumeDown(){
+
 	}
 }
