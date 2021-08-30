e0555dccacec39e1bc36d8af72a41bc6404ad4f1
GoKritz <voter96@gmail.com>
2021-08-30 13:26:39 +1200

    can't focus in living room, moving to desktop

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -3,12 +3,15 @@ package;
 import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.addons.ui.FlxUIButton;
 import flixel.group.FlxGroup;
 import flixel.system.FlxSound;
-import flixel.addons.ui.FlxUIButton;
+import flixel.util.FlxSave;
 
 class MenuState extends FlxState
 {
+	var _musicVolume:Int;
+	var _gameVolume:Int;
 	var _mainMenu:FlxTypedGroup<FlxUIButton>;
 	var _startButton:FlxUIButton;
 	var _newgameButton:FlxUIButton;
@@ -21,12 +24,15 @@ class MenuState extends FlxState
 	var _volumeText:FlxUIButton;
 	var _volUpButton:FlxUIButton;
 	var _volDownButton:FlxUIButton;
+	var _fullscreenButton:FlxUIButton;
 
 	var _menuMusic:FlxSound;
 
 	var _screenWidth:Float;
 	var _screenHeight:Float;
 	var _buttonWidth:Float;
+
+	var save:FlxSave;
 
 	override public function create()
 	{
@@ -62,11 +68,14 @@ class MenuState extends FlxState
 		add(_volUpButton);
 		_volDownButton = new FlxUIButton(_buttonWidth - 80, 90, '-', volumeDown);
 		add(_volDownButton);
+		_fullscreenButton = new FlxUIButton(_buttonWidth, 180, "Fullscreen", fullscreenToggle);
+		add(_fullscreenButton);
 
 		_optionsMenu = new FlxGroup();
 		_optionsMenu.add(_optionsCloseButton);
 		_optionsMenu.add(_volUpButton);
 		_optionsMenu.add(_volDownButton);
+		_optionsMenu.add(_fullscreenButton);
 
 		_mainMenu.forEach(hideButton);
 		_optionsMenu.forEach(hideButton);
@@ -81,77 +90,101 @@ class MenuState extends FlxState
 		FlxG.sound.music = _menuMusic;
 		FlxG.sound.music.play();
 
+		save = new FlxSave();
+		save.bind("exterminationVolumes");
+
+		FlxG.sound.music.volume = save.data.musicVolume;
+
 		super.create();
 	}
 
 	// called in a loop
-	function hideButton(button:FlxBasic){
+	function hideButton(button:FlxBasic)
+	{
 		button.visible = false;
 	}
 
 	// called in a loop
-	function showButton(button:FlxBasic){
+	function showButton(button:FlxBasic)
+	{
 		button.visible = true;
 	}
 
 	// displays the main menu
-	function clickPlay(){
+	function clickPlay()
+	{
 		_startButton.visible = false;
 		_mainMenu.forEach(showButton);
 	}
 
 	// switches to PlayState.hx
-	function newGame(){
+	function newGame()
+	{
+		save.close();
 		FlxG.sound.music.pause();
 		FlxG.switchState(new PlayState());
 	}
 
 	// hides main menu and displays the options menu
-	function optionsMenu(){
+	function optionsMenu()
+	{
 		_mainMenu.forEach(hideButton);
 		_optionsMenu.forEach(showButton);
 	}
 
 	// hides options menu and displays the main menu
-	function closeOptions(){
+	function closeOptions()
+	{
 		_optionsMenu.forEach(hideButton);
 		_mainMenu.forEach(showButton);
 	}
 
-	function continueGame(){
+	function continueGame()
+	{
 		// this doesn't do anything right now
 	}
 
-	function levelsMenu(){
+	function levelsMenu()
+	{
 		// this doesn't do anything right now
 	}
 
-	function volumeUp(){
-
+	function volumeUp()
+	{
+		FlxG.sound.music.volume += 0.1;
+		save.data.musicVolume = FlxG.sound.music.volume;
 	}
 
-	function volumeDown(){
-
+	function volumeDown()
+	{
+		FlxG.sound.music.volume -= 0.1;
+		save.data.musicVolume = FlxG.sound.music.volume;
 	}
 
 	// assigns the graphic to use for the menu buttons
-	function loadButtonGraphic(button:FlxUIButton){
+	function loadButtonGraphic(button:FlxUIButton)
+	{
 		button.loadGraphic("assets/images/ui/bloodbutton.png");
 		/*button.setSize(110, 58);
-		button.setLabel(new FlxUIText(
-			50, 10, 100, button.label.text
-		));*/
+			button.setLabel(new FlxUIText(
+				50, 10, 100, button.label.text
+			)); */
 
 		button.label.offset.set(25, -25);
-		//button.label.setBorderStyle(OUTLINE,FlxColor.TRANSPARENT,40);
-		
-		//button.getLabel().y += 10;
-		
+		// button.label.setBorderStyle(OUTLINE,FlxColor.TRANSPARENT,40);
+
+		// button.getLabel().y += 10;
+
 		/*button.setLabel(new FlxUIText(
-			50, 
-			50,
-			button.getLabel().width,
-			button.getLabel().text)
-		);*/
+				50, 
+				50,
+				button.getLabel().width,
+				button.getLabel().text)
+			); */
+	}
+
+	function fullscreenToggle()
+	{
+		FlxG.fullscreen = !FlxG.fullscreen;
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,20 +1,20 @@
 package;
 
-import pickups.guns.PistolPickup;
-import pickups.guns.RiflePickup;
-import pickups.ammo.RifleAmmo;
-import pickups.Pickup;
-import pickups.ammo.PistolAmmo;
-import flixel.group.FlxGroup;
-import guns.Bullet;
-import npcs.NPC;
-import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.addons.display.FlxBackdrop;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxState;
+import flixel.addons.display.FlxBackdrop;
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxGroup;
 import flixel.tile.FlxTilemap;
+import guns.Bullet;
+import npcs.NPC;
+import pickups.Pickup;
+import pickups.ammo.PistolAmmo;
+import pickups.ammo.RifleAmmo;
+import pickups.guns.PistolPickup;
+import pickups.guns.RiflePickup;
 
 class PlayState extends FlxState
 {
@@ -27,7 +27,7 @@ class PlayState extends FlxState
 	var _npcs:FlxTypedGroup<NPC>;
 	var _pistolBullets:FlxTypedGroup<Bullet>;
 	var _rifleBullets:FlxTypedGroup<Bullet>;
-	
+
 	// pickups
 	var _pistolAmmo:FlxTypedGroup<PistolAmmo>;
 	var _rifleAmmo:FlxTypedGroup<RifleAmmo>;
@@ -37,31 +37,28 @@ class PlayState extends FlxState
 	// helper groups
 	var _objects:FlxGroup;
 	var _pickups:FlxGroup;
-	
+	var _bullets:FlxGroup;
+
 	override public function create()
 	{
 		// set the background image
-		_backdrop = new FlxBackdrop(
-			"assets/images/Backgrounds/backdrop.png", 
-			0.5, 0.5, true, 0, 0);
+		_backdrop = new FlxBackdrop("assets/images/Backgrounds/backdrop.png", 0.5, 0.5, true, 0, 0);
 
 		// load the level data
-		_map = new FlxOgmo3Loader(
-			"assets/levels/hworld.ogmo", 
-			"assets/levels/NewLevel0.json");
+		_map = new FlxOgmo3Loader("assets/levels/hworld.ogmo", "assets/levels/NewLevel0.json");
 		_tilemap = _map.loadTilemap("assets/data/tilewall.png", "walls");
 		_tilemap.follow();
 
 		// declare which blocks are solid and collide with stuff
 		_tilemap.setTileProperties(1, FlxObject.NONE);
 		_tilemap.setTileProperties(2, FlxObject.ANY);
-		
+
 		_npcs = new FlxTypedGroup<NPC>();
-		
+
 		_player = new Player();
-		
+
 		_player.declarePeds(_npcs);
-		
+
 		// === PICKUP DECLARATIONS ===
 		_pistolBullets = new FlxTypedGroup<Bullet>(20);
 		_rifleBullets = new FlxTypedGroup<Bullet>(12);
@@ -70,11 +67,11 @@ class PlayState extends FlxState
 		_rifleAmmo = new FlxTypedGroup<RifleAmmo>();
 		_rifles = new FlxTypedGroup<RiflePickup>();
 		_pistols = new FlxTypedGroup<PistolPickup>();
-		
+
 		_hud = new HUD();
 		_player.hud = _hud;
 		_player.updateHUD();
-		
+
 		// === OBJECTS GROUP
 		_objects = new FlxGroup();
 		_objects.add(_player);
@@ -82,13 +79,18 @@ class PlayState extends FlxState
 		_objects.add(_rifleBullets);
 		_objects.add(_tilemap);
 		_objects.add(_npcs);
-		
+
 		// === PICKUPS GROUP ===
 		_pickups = new FlxGroup();
 		_pickups.add(_pistolAmmo);
 		_pickups.add(_rifleAmmo);
 		_pickups.add(_rifles);
 		_pickups.add(_pistols);
+
+		// === BULLETS GROUP ===
+		_bullets = new FlxGroup();
+		_bullets.add(_pistolBullets);
+		_bullets.add(_rifleBullets);
 
 		_map.loadEntities(placeEntities, "entities");
 		add(_backdrop);
@@ -116,12 +118,14 @@ class PlayState extends FlxState
 		FlxG.sound.volume = 0.2;
 	}
 
-	function placeEntities(entity:EntityData){
-		switch(entity.name){
+	function placeEntities(entity:EntityData)
+	{
+		switch (entity.name)
+		{
 			case "player":
 				_player.setPosition(entity.x, entity.y);
 			case "NPC":
-				_npcs.add(new NPC(entity.x - 16, entity.y -16));
+				_npcs.add(new NPC(entity.x - 16, entity.y - 16));
 			case "pistolammo":
 				_pistolAmmo.add(new PistolAmmo(entity.x, entity.y - 4));
 			case "rifleammo":
@@ -133,42 +137,57 @@ class PlayState extends FlxState
 		}
 	}
 
-	override public function update(elapsed:Float){
+	override public function update(elapsed:Float)
+	{
 		collisions();
 		FlxG.camera.follow(_player, PLATFORMER, 1);
 		super.update(elapsed);
 	}
 
-	function collisions(){
-		//FlxG.collide(_tilemap, _player);
-		FlxG.collide(_objects, _tilemap, objectCollide);
+	function collisions()
+	{
+		// FlxG.collide(_tilemap, _player);
+		// FlxG.collide(_objects, _tilemap, objectCollide);
+
+		/*
+			having issues with the 'is' operator here, works on my desktop
+			but not on my laptop...
+
+			giving them a more specific collide until I figure that shit out
+		 */
+
+		FlxG.collide(_bullets, _tilemap, bulletWall);
 		FlxG.overlap(_npcs, _pistolBullets, npcShot);
 		FlxG.overlap(_npcs, _rifleBullets, riflenpcShot);
 		FlxG.overlap(_player, _pickups, pickupItem);
 	}
 
 	// callback when NPCs are shot
-	function npcShot(sprite1:NPC, sprite2:FlxObject){
-		if(sprite1.health > 0){
+	function npcShot(sprite1:NPC, sprite2:FlxObject)
+	{
+		if (sprite1.health > 0)
+		{
 			sprite2.kill();
 			sprite1.getStabbed();
 		}
 	}
-	
+
 	// unique callback for penetrating shots
-	function riflenpcShot(sprite1:NPC, sprite2:Bullet){
-		if(sprite1.health > 0){
+	function riflenpcShot(sprite1:NPC, sprite2:Bullet)
+	{
+		if (sprite1.health > 0)
+		{
 			sprite1.getStabbed();
 		}
 	}
 
-	function objectCollide(obj1:FlxObject, tmap:FlxTilemap){
-		if(obj1 is Bullet){
-			obj1.kill();
-		}
+	function bulletWall(obj1:FlxObject, tmap:FlxTilemap)
+	{
+		obj1.kill();
 	}
 
-	function pickupItem(player:Player, item:Pickup){
+	function pickupItem(player:Player, item:Pickup)
+	{
 		item.get(player);
 		player.updateHUD();
 	}
